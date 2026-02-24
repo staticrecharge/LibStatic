aa65e43c168d3f43304c36f9b667e34d3c2af871
staticrecharge <staticrecharged@gmail.com>
2026-02-23 18:08:44 -0700

    fixes

--- PairedList.lua
+++ PairedList.lua
@@ -4,7 +4,7 @@ Author:					Static_Recharge
 Description:		Object to manage a paired list of choices and values. Mainly for settings menus.
                 If no Values table is passed in the object will become an enumerated list instead.
 
-PL                                                - Object containing all functions, tables, variables, constants and other data managers.
+PairedList                                                - Object containing all functions, tables, variables, constants and other data managers.
 ├─ :IsEnum()                                      - Returns true if the object is an enum list.
 ├─ :IsInitialized()                               - Returns true if the object has been successfully initialized.
 ├─ :GetChoices()                                  - Returns the Choices table.
@@ -13,7 +13,7 @@ PL                                                - Object containing all functi
 ├─ :UpdateData(Choices, Values)                   - Updates the Choices and Values tables.
 ├─ :Sort(sortType)                                - Sorts the paired list by the specified sortType (use globals).
 ├─ .choice_1 ┐
-¦      ¦     ├─                                   - Allows choices to be used directly, good for enums. Ex: PL.choice_2
+¦      ¦     ├─                                   - Allows choices to be used directly, good for enums. Ex: PairedList.choice_2
 └─ .choice_n ┘
 ------------------------------------------------------------------------------------------------]]--
 
@@ -21,7 +21,7 @@ PL                                                - Object containing all functi
 --[[------------------------------------------------------------------------------------------------
 Globals
 ------------------------------------------------------------------------------------------------]]--
--- for use with PL:Sort
+-- for use with PairedList:Sort
 LIBSTATIC_LIST_SORT_MIN = 1
 LIBSTATIC_LIST_SORT_MAX = 4
 LIBSTATIC_LIST_SORT_CHOICES_ASCENDING = 1
@@ -30,20 +30,20 @@ LIBSTATIC_LIST_SORT_VALUE_ASCENDING = 3
 LIBSTATIC_LIST_SORT_VALUE_DESCENDING = 4
 
 --[[------------------------------------------------------------------------------------------------
-PL Class Initialization
+PairedList Class Initialization
 ------------------------------------------------------------------------------------------------]]--
-local PL = ZO_InitializingObject:Subclass()
+local PairedList = ZO_InitializingObject:Subclass()
 
 
 --[[------------------------------------------------------------------------------------------------
-PL:Initialize()
+PairedList:Initialize()
 Inputs:				Choices                             - Table of choices (enums)
               Values                              - (optional) Table of values
 Outputs:			None
 Description:	Initializes the object. If Values table is missing the object will be treated as an enumerated list.
               Once created as an enum that shouldn't be changed. It's better to destroy the object and start over.
 ------------------------------------------------------------------------------------------------]]--
-function PL:Initialize(Choices, Values)
+function PairedList:Initialize(Choices, Values)
   self.Choices = Choices
   self.Values = Values
   self.isEnum = false
@@ -69,56 +69,56 @@ end
 
 
 --[[------------------------------------------------------------------------------------------------
-PL:IsEnum()
+PairedList:IsEnum()
 Inputs:				None
 Outputs:			isEnum                              - bool for object enum state
 Description:	Returns true if the object is an enum list.
 ------------------------------------------------------------------------------------------------]]--
-function PL:IsEnum()
+function PairedList:IsEnum()
   return self.isEnum
 end
 
 
 --[[------------------------------------------------------------------------------------------------
-PL:IsInitialized()
+PairedList:IsInitialized()
 Inputs:				None
 Outputs:			initialized                         - bool for object initialized state
 Description:	Returns true if the object has been successfully initialized.
 ------------------------------------------------------------------------------------------------]]--
-function PL:IsInitialized()
+function PairedList:IsInitialized()
   return self.initialized
 end
 
 
 --[[------------------------------------------------------------------------------------------------
-PL:GetChoices()
+PairedList:GetChoices()
 Inputs:				None
 Outputs:			Choices                             - Table of indexed Choices
 Description:	Returns the Choices table.
 ------------------------------------------------------------------------------------------------]]--
-function PL:GetChoices()
+function PairedList:GetChoices()
   return self.Choices
 end
 
 
 --[[------------------------------------------------------------------------------------------------
-PL:GetValues()
+PairedList:GetValues()
 Inputs:				None
 Outputs:			Values                              - Table of indexed values
 Description:	Returns the Values table.
 ------------------------------------------------------------------------------------------------]]--
-function PL:GetValues()
+function PairedList:GetValues()
   return self.Values
 end
 
 
 --[[------------------------------------------------------------------------------------------------
-PL:GetChoiceByValue(value)
+PairedList:GetChoiceByValue(value)
 Inputs:				value                               - Value to look up
 Outputs:			choice                              - Choice found by value
 Description:	Returns the matching choice from the paired list.
 ------------------------------------------------------------------------------------------------]]--
-function PL:GetChoiceByValue(value)
+function PairedList:GetChoiceByValue(value)
   local index
   for i, v in ipairs(self.Values) do
     if v == value then
@@ -131,13 +131,13 @@ end
 
 
 --[[------------------------------------------------------------------------------------------------
-PL:UpdateData(Choices, Values)
+PairedList:UpdateData(Choices, Values)
 Inputs:				Choices                             - Table of choices (enums)
               Values                              - (optional) Table of values
 Outputs:			None
 Description:	Updates the Choices and Values tables.
 ------------------------------------------------------------------------------------------------]]--
-function PL:UpdateData(Choices, Values)
+function PairedList:UpdateData(Choices, Values)
   -- clear old info
   if Choices ~= nil then
     for index, choice in ipairs(self.Choices) do
@@ -164,12 +164,15 @@ end
 
 
 --[[------------------------------------------------------------------------------------------------
-PL:Sort(sortType)
+PairedList:Sort(sortType)
 Inputs:				sortType                            - how to sort (globals)
 Outputs:			None
-Description:	Sorts the paired list by the specified sortType (use globals).
+Description:	Sorts the paired list by the specified sortType (use globals). Defaults to LIBSTATIC_LIST_SORT_CHOICES_ASCENDING
 ------------------------------------------------------------------------------------------------]]--
-function PL:Sort(sortType)
+function PairedList:Sort(sortType)
+  -- set default if not specified
+  if sortType == nil then sortType = LIBSTATIC_LIST_SORT_CHOICES_ASCENDING end
+  
   -- check range
   if sortType < LIBSTATIC_LIST_SORT_MIN or sortType > LIBSTATIC_LIST_SORT_MAX then return end
 
@@ -204,4 +207,4 @@ end
 --[[------------------------------------------------------------------------------------------------
 Global template assignment
 ------------------------------------------------------------------------------------------------]]--
-LibStatic.PairedList = PL
+LibStatic.PAIREDLIST = PairedList
